46c0b44b866c3258e4562a8abb362a0684d94219
Yi Nian Niu <yinian.niu@mail.utoronto.ca>
2018-03-17 20:22:55 -0400

    remove files

--- src/RestaurantSimulation.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class RestaurantSimulation {
-
-    public static void main(String[] args) throws IOException {
-        Restaurant restaurant = new Restaurant();
-        restaurant.configureRestaurant("config.txt");
-    }
-}
